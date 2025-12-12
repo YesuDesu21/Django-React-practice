f60c9d5a4fee941b8336b7eb015a9c41470f48a5
jasper <doria.feiljasper@gmail.com>
2025-12-12 20:33:35 +0800

    other dependencies

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,11 @@
 //correct headers to avoid writing many times
 import axios from "axios";
 import {ACCESS_TOKEN} from "./constants";
-
+const apiURL = "https://95fdc41d-cac3-42a5-b6a9-20a207ee40b9-dev.e1-us-east-azure.choreoapis.dev/django-react-practice/backend/v1.0"
 //import anything inside env variable file
 const api = axios.create({
     //url of backend server
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 });
 api.interceptors.request.use(
     (config)=>{
