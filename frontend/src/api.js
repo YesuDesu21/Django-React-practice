7eb20fe74da4b1dee1c8bb150f1bd571ea3cda77
jasper <doria.feiljasper@gmail.com>
2025-12-12 20:47:27 +0800

    fixed apiURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 //correct headers to avoid writing many times
 import axios from "axios";
 import {ACCESS_TOKEN} from "./constants";
-const apiURL = "https://95fdc41d-cac3-42a5-b6a9-20a207ee40b9-dev.e1-us-east-azure.choreoapis.dev/django-react-practice/backend/v1.0"
+const apiURL = "/choreo-apis/django-react-practice/backend/v1"
 //import anything inside env variable file
 const api = axios.create({
     //url of backend server
